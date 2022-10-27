9322e4c4b8502cd698de6d1adb3e046fe59ef90d
Anita Lipsky <anita@purplebugs.com>
2022-10-27 15:34:08 +0200

    one CSS file only, to simplify

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import Home from "./pages/Home";
 import GetPositionGetAlpacas from "./pages/GetPositionGetAlpacas";
 import MapFixedCenter from "./pages/MapFixedCenter";
